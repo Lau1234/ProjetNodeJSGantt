73f0868dabe049656785aae8f78bd913fd50329c
AntoineAcquart <antoine.acquart@gmail.com>
2019-12-16 17:12:40 +0100

    schema format

--- Controller/Mongoose.js
+++ Controller/Mongoose.js
@@ -9,23 +9,41 @@ db.once("open", function() {
   // we're connected!
 });
 
-const Schema = mongoose.Schema; 
-const ganttSchema = new Schema (
-  {
-  nameService : String, 
-  projects : [
- { 
-    name : String, 
-    desc : String, 
-    daysOff : { Mo : Boolean, Tu : Boolean,  We : Boolean, Th : Boolean, Fr : Boolean, Sa : Boolean, Su : Boolean },
-    workingHours : { start : Number, end : Number }, 
-    task : [{ id : Number, name : String, desc : String, start : Number, end : Number, percentageProgress : Number, color  : String, linkedTask : Array, ressources : Array }], 
-    groupTask : [{ name : String, start : Number, end : Number }], 
-    resources : [{ name : String, cost : Number, type : String }], 
-    milestones : [{ name : String, date : Number }] 
-   } 
-  ] 
- }
- );
+const Schema = mongoose.Schema;
+const ganttSchema = new Schema({
+  nameService: String,
+  projects: [
+    {
+      name: String,
+      desc: String,
+      daysOff: {
+        Mo: Boolean,
+        Tu: Boolean,
+        We: Boolean,
+        Th: Boolean,
+        Fr: Boolean,
+        Sa: Boolean,
+        Su: Boolean
+      },
+      workingHours: { start: Number, end: Number },
+      task: [
+        {
+          id: Number,
+          name: String,
+          desc: String,
+          start: Number,
+          end: Number,
+          percentageProgress: Number,
+          color: String,
+          linkedTask: Array,
+          ressources: Array
+        }
+      ],
+      groupTask: [{ name: String, start: Number, end: Number }],
+      resources: [{ name: String, cost: Number, type: String }],
+      milestones: [{ name: String, date: Number }]
+    }
+  ]
+});
 
- module.exports = mongoose.model('gantt', ganttSchema);
+module.exports = mongoose.model("gantt", ganttSchema);
